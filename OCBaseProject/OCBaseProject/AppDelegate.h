97799144ef4f67026539f03d78316f02eae8cc3e
yourks <568986318@qq.com>
2017-04-14 14:50:58 +0800

    重新创建工程 运行后没有问题 提交工程

---
+++ OCBaseProject/OCBaseProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OCBaseProject
+//
+//  Created by Apple on 2017/4/14.
+//  Copyright © 2017年 YKYourks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OCBaseProject/OCBaseProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OCBaseProject
+//
+//  Created by Apple on 2017/4/14.
+//  Copyright © 2017年 YKYourks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
